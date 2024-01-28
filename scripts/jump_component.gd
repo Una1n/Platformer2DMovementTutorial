cc1528e2e56e9bd3d2c000d585f4ea8dcb9a3824
Unain <vanstoel@gmail.com>
2024-01-28 17:11:14 +0100

    Disable Coyote Jump when jumping

--- scripts/jump_component.gd
+++ scripts/jump_component.gd
@@ -26,6 +26,7 @@ func _unhandled_key_input(event: InputEvent) -> void:
 	if event.is_action_pressed("jump") and (character.is_on_floor() or coyote_jump):
 		character.velocity.y = jump_velocity
 		jumping = true
+		coyote_jump = false
 
 	# Variable Jump Height
 	if event.is_action_released("jump") and not character.is_on_floor() and character.velocity.y < 0:
