2c8aadce05a86e6b088527f7b5d8e30f349b5e17
Unain <vanstoel@gmail.com>
2024-01-28 16:04:19 +0100

    Added Variable Jump Height

--- scripts/jump_component.gd
+++ scripts/jump_component.gd
@@ -11,3 +11,7 @@ extends Node
 func _unhandled_key_input(event: InputEvent) -> void:
 	if event.is_action_pressed("jump") and character.is_on_floor():
 		character.velocity.y = jump_velocity
+
+	# Variable Jump Height
+	if event.is_action_released("jump") and not character.is_on_floor() and character.velocity.y < 0:
+		character.velocity.y = 0
