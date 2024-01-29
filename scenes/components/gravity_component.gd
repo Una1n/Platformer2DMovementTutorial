646aa17fb834a3d74325d60f530f4c6509588f71
Unain <vanstoel@gmail.com>
2024-01-29 17:29:07 +0100

    Moved falling to gravity component

--- scenes/components/gravity_component.gd
+++ scenes/components/gravity_component.gd
@@ -5,8 +5,11 @@ extends Node
 @export_subgroup("Settings")
 @export var gravity: float = 1000.0
 
+var is_falling: bool = false
 
 func handle_gravity(body: CharacterBody2D, delta: float) -> void:
 	if not body.is_on_floor():
 		# Need to multiply by delta when adding to velocity
 		body.velocity.y += gravity * delta
+
+	is_falling = body.velocity.y > 0
--- scenes/components/jump_component.gd
+++ scenes/components/jump_component.gd
@@ -5,18 +5,11 @@ extends Node
 @export_subgroup("Settings")
 @export var jump_velocity: float = -350.0
 
-var jumping: bool = false
-var last_frame_on_floor: bool = false
 var is_jumping: bool = false
-var is_falling: bool = false
 
 
 func handle_jump(body: CharacterBody2D, want_to_jump: bool) -> void:
 	if want_to_jump and body.is_on_floor():
 		body.velocity.y = jump_velocity
-		jumping = true
 
-	is_jumping = body.velocity.y < 0
-	is_falling = body.velocity.y > 0
-
-	last_frame_on_floor = body.is_on_floor()
+	is_jumping = body.velocity.y < 0 and not body.is_on_floor()
