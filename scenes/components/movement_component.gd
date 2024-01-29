6c48aca6be3b67f9a70149a40e566995b66022db
Unain <vanstoel@gmail.com>
2024-01-29 17:28:44 +0100

    Set default speed to 100

--- scenes/components/movement_component.gd
+++ scenes/components/movement_component.gd
@@ -3,7 +3,7 @@ extends Node
 
 
 @export_subgroup("Settings")
-@export var speed: float = 150
+@export var speed: float = 100
 
 
 func handle_horizontal_movement(body: CharacterBody2D, direction: float) -> void:
