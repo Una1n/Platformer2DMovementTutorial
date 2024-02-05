c4be00c1024206af4c9147bc88f2939bdee51103
Unain <vanstoel@gmail.com>
2024-02-05 19:29:01 +0100

    Updated Movement Lerp to move_toward

--- scenes/components/movement_component.gd
+++ scenes/components/movement_component.gd
@@ -4,10 +4,10 @@ extends Node
 
 @export_subgroup("Settings")
 @export var speed: float = 100
-@export var ground_accel_speed: float = 0.7
-@export var ground_decel_speed: float = 0.5
-@export var air_accel_speed: float = 0.3
-@export var air_decel_speed: float = 0.1
+@export var ground_accel_speed: float = 6.0
+@export var ground_decel_speed: float = 8.0
+@export var air_accel_speed: float = 10.0
+@export var air_decel_speed: float = 3.0
 
 
 func handle_horizontal_movement(body: CharacterBody2D, direction: float) -> void:
@@ -17,4 +17,4 @@ func handle_horizontal_movement(body: CharacterBody2D, direction: float) -> void
 	else:
 		velocity_change_speed = air_accel_speed if direction != 0 else air_decel_speed
 
-	body.velocity.x = lerpf(body.velocity.x, direction * speed, velocity_change_speed)
+	body.velocity.x = move_toward(body.velocity.x, direction * speed, velocity_change_speed)
