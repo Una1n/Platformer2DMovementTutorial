c9d5ddbd9d8a41b9226f3546de59e71f2bc519b0
Unain <vanstoel@gmail.com>
2024-02-05 19:30:27 +0100

    Updated coyote time to be 5 frames

--- scenes/components/gravity_component.gd
+++ scenes/components/gravity_component.gd
@@ -8,7 +8,6 @@ var is_falling: bool = false
 
 func handle_gravity(body: CharacterBody2D, delta: float) -> void:
 	if not body.is_on_floor():
-		# Need to multiply by delta when adding to velocity
 		body.velocity.y += gravity * delta
 
 	is_falling = body.velocity.y > 0 and not body.is_on_floor()
