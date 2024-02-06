c7ebca2d0ea50cdd680bce7683d0015f2a8338cc
Unain <vanstoel@gmail.com>
2024-02-06 18:24:19 +0100

    Refactored Advanced Jump Component

--- scenes/components/advanced_jump_component.gd
+++ scenes/components/advanced_jump_component.gd
@@ -12,23 +12,34 @@ var is_going_up: bool = false
 var is_jumping: bool = false
 var last_frame_on_floor: bool = false
 
+
+func has_just_landed(body: CharacterBody2D) -> bool:
+	return body.is_on_floor() and not last_frame_on_floor and is_jumping
+
+
+func is_allowed_to_jump(body: CharacterBody2D, want_to_jump: bool) -> bool:
+	return want_to_jump and (body.is_on_floor() or not coyote_timer.is_stopped())
+
+
 func handle_jump(body: CharacterBody2D, want_to_jump: bool, jump_released: bool) -> void:
-	if body.is_on_floor() and not last_frame_on_floor and is_jumping:
+	if has_just_landed(body):
 		is_jumping = false
 
-	if want_to_jump and body.is_on_floor():
+	if is_allowed_to_jump(body, want_to_jump):
 		jump(body)
 
-	handle_coyote_time(body, want_to_jump)
+	handle_coyote_time(body)
 	handle_jump_buffer(body, want_to_jump)
 	handle_variable_jump_height(body, jump_released)
 
 	is_going_up = body.velocity.y < 0 and not body.is_on_floor()
 	last_frame_on_floor = body.is_on_floor()
 
+
 func handle_variable_jump_height(body: CharacterBody2D, jump_released: bool) -> void:
 	if jump_released and is_going_up:
 		body.velocity.y = 0
+
 
 func handle_jump_buffer(body: CharacterBody2D, want_to_jump: bool) -> void:
 	if want_to_jump and not body.is_on_floor():
@@ -37,16 +48,18 @@ func handle_jump_buffer(body: CharacterBody2D, want_to_jump: bool) -> void:
 	if body.is_on_floor() and not jump_buffer_timer.is_stopped():
 		jump(body)
 
-func handle_coyote_time(body: CharacterBody2D, want_to_jump: bool) -> void:
-	if not body.is_on_floor() and last_frame_on_floor and not is_jumping:
+
+func has_just_stepped_off_ledge(body: CharacterBody2D) -> bool:
+	return not body.is_on_floor() and last_frame_on_floor and not is_jumping
+
+
+func handle_coyote_time(body: CharacterBody2D) -> void:
+	if has_just_stepped_off_ledge(body):
 		coyote_timer.start()
 
-	if want_to_jump and not body.is_on_floor() and not coyote_timer.is_stopped():
-		jump(body)
-
-	# Dont drop down right at the edge when walking, keep us in the air for the coyote time
 	if not coyote_timer.is_stopped() and not is_jumping:
 		body.velocity.y = 0
+
 
 func jump(body: CharacterBody2D) -> void:
 	body.velocity.y = jump_velocity
